77675349349c7b5e7780099bf5c61145c23bbc29
Geetha Vangala <geetha.vangala@gmail.com>
2023-01-24 16:40:05 -0500

    my seventh commit

--- test_hello.py5.py
+++ test_hello.py5.py
@@ -1,2 +1,8 @@
+
+#this is an example
 print ("Hello World");
 print("This is my sixth program");
+first_name = 'Venkat';
+last_name = 'vangala';
+full_name = first_name + ' ' + last_name;
+print(full_name);

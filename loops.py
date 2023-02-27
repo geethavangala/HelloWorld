d4003b0c92b2b9ad7cc0f22f76275d483658d5dc
Geetha Vangala <geetha.vangala@gmail.com>
2023-02-26 21:25:24 -0500

    project plan

--- loops.py
+++ loops.py
@@ -1,11 +1,3 @@
-i = 2
-while i < 50:
-    if i%2 !=0:
-        print(i)
-    i = i+1
-
-sum=0
-print ("first ten natural numbers are")
 n =10
 sum = n*(n+1)/2
-print (sum)
+print ("Sum of first ten natural numbers is:", sum)
